5027ed80b02ae5cbdd5e9d14129ae15417e6eefe
AshaMohamed68 <ashaabdullahi5968@gmail.com>
2025-07-06 22:38:03 +0300

    we learnt loops

---
+++ index.js
@@ -0,0 +1,48 @@
+// types of loo
+// 1. for
+
+// 2. while
+
+//3. do while
+
+
+// LOOPS
+// console.log("heelo world");
+// console.log("heelo world");
+// console.log("heelo world");
+// console.log("heelo world");
+// console.log("heelo world");
+// console.log("heelo world");
+
+// Types Of LOOPS
+//1. For LOOP
+//2. While LOOP
+//3. Do While Loop
+
+//1. For LOOP
+for (let i = 50; i <= 100; i++){
+ console.log(i);
+ }
+
+// 2. While Loop
+ let xisaab = 1;
+ while(xisaab <= 10){
+ console.log(xisaab);
+ xisaab++;
+ }
+
+//3. Do While Loop
+let i =10;
+ do{
+ console.log(i);
+ i++
+ }while(i <=20);
+
+
+for (let love = 1; love <= 100; love++){
+    console.log(love, "i love you ❤️");
+}
+
+
+
+
